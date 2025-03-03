90d4a10755f4917374e4f0c70c0038bf011332e1
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-03-03 14:30:12 -0500

    Creo un componente de lista de treas reactivas utilizando ref, v-model, v-for y directivas

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
 
 <MiContador></MiContador>
-
+<ListaDeTareas></ListaDeTareas>
 </template>
 
 <script setup>
 
 import MiContador from './components/MiContador.vue';
+import ListaDeTareas from './components/ListaDeTareas.vue';
 
 </script>
 
