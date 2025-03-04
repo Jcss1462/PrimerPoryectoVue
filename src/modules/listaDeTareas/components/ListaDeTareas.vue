13754a1b113781eb306ec03a235489df9be0d5dc
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2025-03-04 00:10:05 -0500

    Se crea store con piia y se escuchan los cambios desde otro componente (se deja documentada alternativa cuando las cariable no son reactivas)

--- src/modules/listaDeTareas/components/ListaDeTareas.vue
+++ src/modules/listaDeTareas/components/ListaDeTareas.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
         <h2>Lista de tareas</h2>
+        <h3>Nombre: {{ nombre }}</h3>
+        <h3>Email: {{ email }}</h3>
         <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
         <ul>
             <li v-for="(tarea, index) in tareas" :key="index">{{tarea}}  <button @click="borrarTarea(index)">Borrar</button></li>
@@ -11,9 +13,12 @@
 </template>
   
 <script setup>
-    import { ref } from 'vue';
+    import { useRegistrarStore } from '@/modules/registro/stores/registrarStores';
+import { ref, watch } from 'vue';
 
-
+    const registrarStore = useRegistrarStore();
+    const nombre =registrarStore.nombre;
+    const email =registrarStore.email;
     const tareas = ref([]);
     const nuevaTarea = ref("");
 
@@ -25,6 +30,15 @@
     const borrarTarea = (index) =>{
         tareas.value.splice(index,1);
     }
+
+    // // Escuchar cambios en la store y actualizar los valores locales
+    // watch(() => registrarStore.nombre, (newValue) => {
+    //     nombre.value = newValue;
+    // });
+
+    // watch(() => registrarStore.email, (newValue) => {
+    //     email.value = newValue;
+    // });
 
 
 </script>
--- src/modules/registro/views/RegistroVue.vue
+++ src/modules/registro/views/RegistroVue.vue
@@ -4,13 +4,13 @@
         <Form :validation-schema="schema" @submit="onSubmit">
             <div class="form">
                 <label for="nombre">Nombre:</label>
-                <Field type="text" name="nombre"  id="nombre" placeholder="Ingrese su nombre"/>
+                <Field v-model="nombre" type="text" name="nombre"  id="nombre" placeholder="Ingrese su nombre"/>
                 <br>
                 <ErrorMessage name="nombre"></ErrorMessage>
             </div>
             <div class="form">
                 <label for="email">Email:</label>
-                <Field type="email" name="email" id="email" placeholder="Ingrese su email"/>
+                <Field v-model="email" type="email" name="email" id="email" placeholder="Ingrese su email"/>
                 <br>
                 <ErrorMessage name="email"></ErrorMessage>
             </div>
@@ -23,8 +23,15 @@
 <script setup>
     import {Form, Field, ErrorMessage} from 'vee-validate';
     import { schema } from '../schemas/validationSchema';
+    import { useRegistrarStore } from '../stores/registrarStores';
+    import { ref } from 'vue';
 
+    const nombre = ref("")
+    const email = ref("")
+
+    const registrarStore = useRegistrarStore();
     const onSubmit = () =>{
+        registrarStore.guardarRegistro(nombre.value,email.value)
         console.log("se ha enviado el formulario");
     }
 
